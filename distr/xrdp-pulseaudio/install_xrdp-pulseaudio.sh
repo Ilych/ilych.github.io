d5d16cb282eb22d824c16ac8c2a51f61b32e3544
Nikolay <nik@i3-host>
2021-11-25 20:36:42 +0300

    fix

--- distr/xrdp-pulseaudio/install_xrdp-pulseaudio.sh
+++ distr/xrdp-pulseaudio/install_xrdp-pulseaudio.sh
@@ -2,14 +2,14 @@
 
 module_dir="$1"
 
-/usr/bin/mkdir -p '/usr/lib/pulse-14.2/modules'
-install -c "$module_dir/module-xrdp-sink.so" /usr/lib/pulse-14.2/modules
-install -c "$module_dir/module-xrdp-sink.la" /usr/lib/pulse-14.2/modules
-install -c "$module_dir/module-xrdp-source.so" /usr/lib/pulse-14.2/modules
-install -c "$module_dir/module-xrdp-source.la" /usr/lib/pulse-14.2/modules
-ldconfig -n  '/usr/lib/pulse-14.2/modules'
+/usr/bin/mkdir -vp '/usr/lib/pulse-14.2/modules'
+installi -v -c "$module_dir/module-xrdp-sink.so" /usr/lib/pulse-14.2/modules
+install -v -c "$module_dir/module-xrdp-sink.la" /usr/lib/pulse-14.2/modules
+install -v -c "$module_dir/module-xrdp-source.so" /usr/lib/pulse-14.2/modules
+install -v -c "$module_dir/module-xrdp-source.la" /usr/lib/pulse-14.2/modules
+ldconfig -vn '/usr/lib/pulse-14.2/modules'
 
-/usr/bin/mkdir -p '/usr/libexec/pulseaudio-module-xrdp'
-/usr/bin/install -c "$module_dir/load_pa_modules.sh" '/usr/libexec/pulseaudio-module-xrdp'
-/usr/bin/mkdir -p '/etc/xdg/autostart'
-/usr/bin/install -c -m 644 "$module_dir/pulseaudio-xrdp.desktop" '/etc/xdg/autostart
+/usr/bin/mkdir -vp '/usr/libexec/pulseaudio-module-xrdp'
+/usr/bin/install -v -c "$module_dir/load_pa_modules.sh" '/usr/libexec/pulseaudio-module-xrdp'
+/usr/bin/mkdir -vp '/etc/xdg/autostart'
+/usr/bin/install -v -c -m 644 "$module_dir/pulseaudio-xrdp.desktop" '/etc/xdg/autostart'
