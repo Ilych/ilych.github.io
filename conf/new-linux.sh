058c76e5e7b7751fd06a031661f340affa56cae4
U-DELL\nik <ilns@dell>
2021-12-24 01:20:02 +0300

    get_help

--- conf/new-linux.sh
+++ conf/new-linux.sh
@@ -9,15 +9,22 @@ homedir="/home/$username"
 scriptdir=$(dirname "$0")
 workdir=$(readlink -f .)
 
-if [ $# -eq "$NO_ARGS" ]  # Сценарий вызван без аргументов?
-then
+get_help() 
+{
   echo "Использование: `basename $0` -acdiu"
   echo " -a   выполнить все операции"
   echo " -c   не обновлять конфиги"
   echo " -d   не скачивать conf"
+  echo " -h   порядок использования"
   echo " -i   не устанавливать wget"
   echo " -u   не создавать пользователя" 
-  
+
+}
+
+
+if [ $# -eq "$NO_ARGS" ]  # Сценарий вызван без аргументов?
+then
+  get_help
   exit $E_OPTERROR        # Если аргументы отсутствуют -- выход с сообщением
                           # о порядке использования скрипта
 fi
@@ -25,7 +32,7 @@ fi
 # Все опции включены по умолчанию
 opt_c=1; opt_d=1; opt_i=1; opt_u=1;
 
-while getopts "acdiu" Option
+while getopts "acdhiu" Option
 do
   case $Option in 
     a ) ;;
@@ -33,8 +40,16 @@ do
     d ) opt_d=0;;
     i ) opt_i=0;;
     u ) opt_u=0;;
+    h ) opt_h=1;;
   esac
 done
+
+if [ "$opt_h" == "1" ]
+then
+  get_help
+  exit 0
+fi
+
 
 if [ "$opt_u" == "1" ]
 then
@@ -56,7 +71,7 @@ then
   echo "Downloading conf.tgz"
   cd "$homedir" || { echo "Can't enter to /home/nik" 1>&2; exit 1; }
   wget https://ilych.github.io/conf.tgz || { echo "Error while download conf.tgz" >&2; exit 3; } 
-  tar -xvf "$homedir"conf.tgz
+  tar -xvf "$homedir/conf.tgz"
   cd "$workdir"
 
 fi
@@ -67,6 +82,8 @@ then
   echo "Updating dot files"
   bash "$scriptdir/set-user.sh" "$homedir" 
 fi
- 
+
+
+
 
 exit 0
